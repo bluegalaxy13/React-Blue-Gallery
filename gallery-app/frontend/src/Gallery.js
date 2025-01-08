1733654f8b27ba1bff4c3e902c3d223bf9e6063e
R Sass <83635268+bluegalaxy13@users.noreply.github.com>
2025-01-08 19:20:15 +0200

    Update Gallery.js (Made it look more organised)

--- gallery-app/frontend/src/Gallery.js
+++ gallery-app/frontend/src/Gallery.js
@@ -505,548 +505,272 @@ const Gallery = () => {
         body: JSON.stringify({ caption }),
       });
 
- 
-
       const data = await response.json();
 
- 
-
       if (data.success) {
-
         notification('Caption added/updated successfully!');
-
         setImages(
-
           images.map((img) =>
-
             img.name === imageName ? { ...img, caption } : img
-
           )
-
         );
-
       } else {
-
         notification('Error adding/updating caption: ' + data.error);
-
       }
-
     } catch (error) {
-
       console.error('Error adding/updating caption:', error);
-
       notification('Error adding/updating caption.');
-
     }
-
   };
-
- 
 
   const deleteCaption = async (imageName) => {
-
     try {
-
       const response = await fetch(`http://localhost:3001/update/caption/${imageName}`, {
-
         method: 'PUT',
-
         headers: { 'Content-Type': 'application/json' },
-
         body: JSON.stringify({ caption: '' }),
-
       });
-
- 
 
       const data = await response.json();
 
- 
-
       if (data.success) {
-
         notification('Caption deleted successfully!');
-
         setImages(
-
           images.map((img) =>
-
             img.name === imageName ? { ...img, caption: '' } : img
-
           )
-
         );
-
       } else {
-
         notification('Error deleting caption: ' + data.error);
-
       }
-
     } catch (error) {
-
       console.error('Error deleting caption:', error);
-
       notification('Error deleting caption.');
-
     }
-
   };
-
- 
-
- 
 
   const handleCaptionSubmit = (event) => {
-
     if (event.key === 'Enter') {
-
       if (newCaption.trim()) {
-
         addCaption(images[currentImageIndex]?.name, newCaption);
-
         setIsEditingCaption(false);
-
       } else {
-
         notification('Caption cannot be empty');
-
       }
-
     }
-
   };
-
- 
-
-  
 
   const handleArrowKeys = useCallback((event) => {
-
     if (event.key === 'ArrowLeft' && currentImageIndex > 0) {
-
       setCurrentImageIndex(currentImageIndex - 1);
-
     } else if (event.key === 'ArrowRight' && currentImageIndex < images.length - 1) {
-
       setCurrentImageIndex(currentImageIndex + 1);
-
     }
-
   }, [currentImageIndex, images.length]);
 
- 
-
- 
-
   const updateExpandedImage = useCallback(() => {
-
     const expandedImage = document.getElementById('expanded-image');
-
     expandedImage.src = 'http://localhost:3001/images/' + images[currentImageIndex]?.name;
-
   }, [currentImageIndex, images]);
 
- 
-
   useEffect(() => {
-
     updateExpandedImage();
-
   }, [currentImageIndex, updateExpandedImage]);
 
- 
-
   useEffect(() => {
-
     loadImages();
-
     document.addEventListener('keydown', handleArrowKeys);
-
     return () => {
-
       document.removeEventListener('keydown', handleArrowKeys);
-
     };
-
   }, [handleArrowKeys, loadImages]);
 
- 
-
   useEffect(() => {
-
     updateExpandedImage();
-
   }, [currentImageIndex, updateExpandedImage]);
 
- 
-
   useEffect(() => {
-
     setIsEditingCaptionInModal(false);
-
     setNewCaption(images[currentImageIndex]?.caption || '');
-
   }, [currentImageIndex, images]);
 
- 
-
   const prevStyle = {
-
     fontSize: '50px',
-
     backgroundColor: 'rgba(255, 255, 255, 0.5)', 
-
     borderRadius: '50%', 
-
     border: 'none',
-
     color: 'white',
-
     position: 'fixed',
-
     top: '50%',
-
     left: '160px', 
-
     transform: 'translateY(-50%)',
-
     zIndex: 10,
-
     width: '60px',
-
     height: '60px',
-
     display: 'flex',
-
     alignItems: 'center',
-
     justifyContent: 'center',
-
     padding: '10px',
-
     boxSizing: 'border-box',
-
     paddingBottom: '20px'
-
   };
-
- 
 
   const nextStyle = {
-
     fontSize: '50px',
-
     backgroundColor: 'rgba(255, 255, 255, 0.5)', 
-
     borderRadius: '50%', 
-
     border: 'none',
-
     color: 'white',
-
     position: 'fixed',
-
     top: '50%',
-
     right: '160px', 
-
     transform: 'translateY(-50%)',
-
     zIndex: 10,
-
     width: '60px',
-
     height: '60px',
-
     display: 'flex',
-
     alignItems: 'center',
-
     justifyContent: 'center',
-
     padding: '10px',
-
     boxSizing: 'border-box',
-
     paddingBottom: '20px',
-
   };
-
- 
 
   const modalStyles = {
-
     modal: {
-
       border: 'none',
-
       boxShadow: 'none',
-
     },
-
     modalDialog: {
-
       border: 'none',
-
       boxShadow: 'none',
-
     },
-
     modalContent: {
-
       border: 'none',
-
       boxShadow: 'none',
-
     },
-
     modalBody: {
-
       padding: 0,
-
     },
-
     expandedImage: {
-
       border: 'none',
-
       width: '100%',
-
       height: 'auto',
-
       objectFit: 'contain',
-
       display: 'block',
-
       margin: '0 auto',
-
     },
-
   };
 
- 
-
   const addCaptionStyle = {
-
     fontSize: '20px',
-
     background: 'none',
-
     borderRadius: '50%',
-
     border: 'none',
-
     color: 'white',
-
     position: 'absolute',
-
     bottom: '10%',
-
     left: '50%',
-
     transform: 'translateX(-50%)',
-
     zIndex: 10,
-
     width: '60px',
-
     height: '60px',
-
     display: 'flex',
-
     alignItems: 'center',
-
     justifyContent: 'center',
-
     padding: '10px',
-
   }
 
- 
-
   return (
-
     <div className="gallery-container">
-
       <div className="content">
-
         <div className="container-fluid mt-4">
-
           <div id="image-gallery">
-
           {imagesToRender.map((img, index) => (
-
             <RenderImageItem
-
               key={img.name}
-
               img={img}
-
               index={index}
-
               deleteCaption={deleteCaption}
-
               addCaption={addCaption}
-
               deleteImage={deleteImage}
-
               getRenderLimit={getRenderLimit}
-
             />
-
           ))}
-
           </div>
-
         </div>
-
       </div>
-
- 
 
       <div  className="modal fade" id="imageModal" tabIndex="-1" aria-labelledby="imageModalLabel" aria-hidden="true">
-
         <div className="modal-dialog modal-dialog-centered" style={modalStyles.modalDialog}>
-
           <div  className="modal-content" style={modalStyles.modalContent}>
-
             <div className="modal-body" style={modalStyles.modalBody}>
-
               <img  id="expanded-image" src="" alt="" />
 
-             
-
               <button
-
                 className="delete-button-expanded"
-
                 onClick={() => {
-
                   const imageName = images[currentImageIndex]?.name;
-
                   deleteImage(imageName);
-
                   const modal = Modal.getInstance(document.getElementById('imageModal'));
-
                   modal.hide();
-
                 }}
-
               >
-
                 X
-
               </button>
-
               <button
-
                 className="prev-btn"
-
                 style={prevStyle}
-
                 onClick={() => currentImageIndex > 0 && setCurrentImageIndex(currentImageIndex - 1)}
-
               >
-
                 &lt;
-
               </button>
-
               <button
-
                 className="next-btn"
-
                 style={nextStyle}
-
                 onClick={() => currentImageIndex < images.length - 1 && setCurrentImageIndex(currentImageIndex + 1)}
-
               >
-
                 &gt;
-
               </button>
-
- 
 
               {!images[currentImageIndex]?.caption && !isEditingCaptionInModal && (
-
                 <button
-
                   className="modal-caption-button"
-
                   style={addCaptionStyle}
-
                   onClick={() => setIsEditingCaptionInModal(true)}
-
                 >
-
-                  Add Caption+
-
+                  Add Caption +
                 </button>
-
               )}
-
- 
 
               {images[currentImageIndex]?.caption && !isEditingCaptionInModal && (
-
                 <span
-
                   className="image-caption"
-
                   onClick={() => {
-
                     setIsEditingCaptionInModal(true);
-
                     setNewCaption(images[currentImageIndex]?.caption);
-
                   }}
-
                 >
-
                   {images[currentImageIndex]?.caption}
-
                 </span>
-
               )}
-
- 
 
               {isEditingCaptionInModal && (
-
                 <input
-
                   type="text"
-
                   value={newCaption}
-
                   onChange={(e) => setNewCaption(e.target.value)}
-
                   onKeyDown={handleCaptionSubmit}
-
                   autoFocus
-
                   className="caption-input"
-
                 />
-
               )}
-
             </div>
-
           </div>
-
         </div>
-
       </div>
-
     </div>
-
   );
-
 };
-
- 
 
 export default Gallery;
